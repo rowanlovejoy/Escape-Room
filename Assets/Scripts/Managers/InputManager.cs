4bd0a23c2cebcf785c168947bc2f6b805b121611
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-01-11 21:46:42 +0000

    Implemented Manager subclass: InputManager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,12 +1,89 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class InputManager : MonoBehaviour 
+public class InputManager : Manager 
 {
     private static InputManager m_instance = null;
 
-	// Update is called once per frame
-	void Update() 
+    private bool m_quit = false;
+    private bool m_confirm = false;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
+
+    private void Awake()
+    {
+        InitialiseManager();
+        SetReferences();
+    }
+
+    protected override void InitialiseManager()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+        } else if (m_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected override void OnSceneLoad(Scene _scene, LoadSceneMode _mode)
+    {
+        SetReferences();
+    }
+
+    protected override void SetReferences()
+    {
+        Debug.Log(NO_REFERENCES_MESSAGES, gameObject);
+    }
+
+    private void Update() 
 	{
-		
+        SetInputs();
 	}
+
+    private void SetInputs()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            m_quit = true;
+        }
+        else
+        {
+            m_quit = false;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_confirm = true;
+        }
+        else
+        {
+            m_confirm = false;
+        }
+    }
+
+    public bool Confirm
+    {
+        get
+        {
+            return m_confirm;
+        }
+    }
+
+    public bool Quit
+    {
+        get
+        {
+            return m_quit;
+        }
+    }
 }
